a2e8b3c9c65f182339b80ca2cf6bcdc1528cffbe
Brice Setra Robert <brice-setra.robert@eurecom.fr>
2024-05-17 09:39:35 +0000

    Update source.sh

--- bin/source.sh
+++ bin/source.sh
@@ -2,7 +2,7 @@ export PATH=${PATH}:/packages/LabSoC/ghdl/bin
 export PATH=${PATH}:/packages/LabSoC/Mentor/Models/bin
 export PATH=${PATH}:/packages/LabSoC/Xilinx/bin
 
-export ds=${HOME}/Developer/ds
+export ds=${HOME}/Developer/ds-sms4
 export sim=/tmp/${USER}/ds/ghdl
 export syn=/tmp/${USER}/ds/syn
 
