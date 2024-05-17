c9911f293dae1f187c098f50ccffb2bb5dd01e60
brobert <brobert@valiha.com>
2024-05-17 10:43:22 +0200

    .

---
+++ bin/source.sh
@@ -0,0 +1,8 @@
+export PATH=${PATH}:/packages/LabSoC/ghdl/bin
+export PATH=${PATH}:/packages/LabSoC/Mentor/Models/bin
+export PATH=${PATH}:/packages/LabSoC/Xilinx/bin
+
+export ds=${HOME}/Developer/ds
+export sim=/tmp/${USER}/ds/ghdl
+export syn=/tmp/${USER}/ds/syn
+
