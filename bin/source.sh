04420b45a5889b7aa5164a84377768f5924c51a4
Brice <robert@eurecom.fr>
2024-05-17 11:50:00 +0200

    .

--- bin/source.sh
+++ bin/source.sh
@@ -1,6 +1,8 @@
 export PATH=${PATH}:/packages/LabSoC/ghdl/bin
 export PATH=${PATH}:/packages/LabSoC/Mentor/Models/bin
 export PATH=${PATH}:/packages/LabSoC/Xilinx/bin
+
 export ds=${HOME}/Developer/ds-sms4
-export sim=/tmp/${USER}/ds/ghdl
-export syn=/tmp/${USER}/ds/syn
+export sim=/tmp/${USER}/ds-sms4/ghdl
+export syn=/tmp/${USER}/ds-sms4/syn
+
