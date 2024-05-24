c157c07722859b384f4d57575d958c22cb45533e
brobert <brobert@valiha.com>
2024-05-24 11:49:51 +0200

    Commenting

--- vhdl/crypto/tb_F_function_round1.vhd
+++ vhdl/crypto/tb_F_function_round1.vhd
@@ -17,6 +17,8 @@ begin
 
     -- Instantiate the Unit Under Test (UUT)
     process
+        constant X_0 : std_ulogic_vector(31 downto 0) := x"27fad345";
+        constant X_1 : std_ulogic_vector(31 downto 0) := x"a18b4cb2";
     begin
         -- Test vector 1
         F_in <= x"0123456789abcdeffedcba9876543210";
@@ -26,7 +28,8 @@ begin
         wait for 10 ns;
 
         -- Display result
-        assert F_out = x"89ABCDEFFEDCBA987654321027FAD345" report "Mismatch for input x'27fad345'" severity failure;
+        assert F_out(31 downto 0) = X_0 
+        report "Mismatch for input x'27fad345'" severity failure;
         report "Test Vector 1: F_in = " & to_hstring(F_in);
         report "Test Vector 1: F_out = " & to_hstring(F_out);
 
@@ -38,7 +41,8 @@ begin
         wait for 10 ns;
 
         -- Display result
-        assert F_out = x"FEDCBA987654321027FAD345A18B4CB2" report "Mismatch for input x'a18b4cb2'" severity failure;
+        assert F_out(31 downto 0) = X_1 
+        report "Mismatch for input x'a18b4cb2'" severity failure;
         report "Test Vector 2: F_in = " & to_hstring(F_in);
         report "Test Vector 2: F_out = " & to_hstring(F_out);
 
